e98052fa8872bb3acda02b096a19eac969237419
rei-rala <ramonirala@outlook.com>
2024-06-08 12:48:33 -0300

    StyledButton vertical center

--- components/ui/StyledButton.jsx
+++ components/ui/StyledButton.jsx
@@ -60,6 +60,7 @@ const styles = StyleSheet.create({
     shadowOffset: { width: 0, height: 4 },
     shadowOpacity: 0.3,
     shadowRadius: 4,
+    justifyContent: "center",
   },
   pressed: {
     shadowColor: 'rgba(0, 0, 0, 0.2)',
